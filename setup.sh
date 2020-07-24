70dc36c0425558d276b2dc90d91ca120432769c3
E_Edelstein <edelsteinevan@gmail.com>
2020-07-24 12:34:37 -0400

    commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-# mkdir -p /Users/evanedelstein/Desktop/Research_Evan/Streamlitapptest/.streamlit/
+
+mkdir -p ~Desktop/Research_Evan/Streamlitapptest/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > /Users/evanedelstein/Desktop/Research_Evan/Streamlitapptest/.streamlit/config.toml
+" > ~/Desktop/Research_Evan/Streamlitapptest/.streamlit/config.toml
