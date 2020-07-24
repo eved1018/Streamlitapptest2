8e60f64f41577c226d94945758804fe4479a47f6
E_Edelstein <edelsteinevan@gmail.com>
2020-07-24 11:57:21 -0400

    commit

---
+++ Streamlitapptest.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+def main():
+    st.title("app")
+    st.subheader("streamlit is cool")
+
+
+if __name__ == '_main_':
+    main() 
---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p /Users/evanedelstein/Desktop/Research_Evan/Streamlitapptest/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > /Users/evanedelstein/Desktop/Research_Evan/Streamlitapptest/.streamlit//config.toml
